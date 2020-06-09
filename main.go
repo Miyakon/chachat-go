1496cfc3dab33e92a5917bb4f5cc7ffd607b1f7b
Miyakon <miyako0901t@gmail.com>
2020-06-09 09:29:33 +0900

    commit

--- main.go
+++ main.go
@@ -6,10 +6,17 @@ import (
 )
 
 func main() {
+	condition := 15
+	if 10 == condition {
+		println("abcd")
+	} else {
+		println("not")
+	}
+
 	//ディレクトリを指定する
 	fs := http.FileServer(http.Dir("static"))
 	//ルーティング設定。"/"というアクセスがきたらstaticディレクトリのコンテンツを表示
-	http.Handle("/", fs) 
+	http.Handle("/", fs)
 
 	log.Println("Listening...")
 	//3000ポートでサーバを立ち上げる
